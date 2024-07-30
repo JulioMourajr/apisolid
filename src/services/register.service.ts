1fefeb4b61a491f181159049ee00f0ccafcb32af
Julio Moura <juliomourajr@hotmail.com>
2024-07-30 04:09:34 -0300

    feat: update services

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
+import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
-import { FastifyReply } from "fastify";
 
 interface RegisterServiceProps {
     name: string;
@@ -8,18 +8,32 @@ interface RegisterServiceProps {
     password: string;
 }
 
-export async function registerService({ name, email, password }: RegisterServiceProps) {
-    const password_hash = await hash(password, 6);
+export class RegisterService {
 
-    const userAlreadyExists = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    });
+    constructor(private usersRepositoty:UsersRepository) {}
 
-    if (userAlreadyExists) {
-        throw new Error("Email already exists")
-    }
+    async registerService({ name, email, password }: RegisterServiceProps) {
+        const password_hash = await hash(password, 6);
     
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+    
+        if (userAlreadyExists) {
+            throw new Error("Email already exists")
+        }
+    
+        await this.usersRepositoty.create({
+            name,
+            email,
+            password_hash
+        })
+        
+    }
+
 }
+
+
 

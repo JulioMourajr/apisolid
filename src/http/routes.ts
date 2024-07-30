da515f18f6f0ca83b15f1d75f758328c65488496
Julio Moura <juliomourajr@hotmail.com>
2024-07-30 04:32:47 -0300

    feat: updated routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
+import { get } from "./controllers/get";
 
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/users", register)
+    app.get("/users", get)
 }
+
+

12d3f9ce2eb23aafddc817942bf7cf8d1bfcb105
Julio Moura <juliomourajr@hotmail.com>
2024-07-30 02:59:19 -0300

    upando os arquivos

---
+++ src/services/register.service.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import { FastifyReply } from "fastify";
+
+interface RegisterServiceProps {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export async function registerService({ name, email, password }: RegisterServiceProps) {
+    const password_hash = await hash(password, 6);
+
+    const userAlreadyExists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (userAlreadyExists) {
+        throw new Error("Email already exists")
+    }
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash
+        },
+    })
+    
+}
+
